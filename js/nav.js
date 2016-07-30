b1dd2505ded07c2a2ab68b7f6a0dd89bd13c4088
Isaac <isaachill13@gmail.com>
2016-07-30 18:54:50 +1000

    added to main.css

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+    $(".burger").click(function() {
+      if ($(".sidebar").hasClass('on')) {
+        $(".sidebar").removeClass("on");
+      } else {
+        $(".sidebar").addClass("on");
+      }
+    });
+
+    $(".burger").on('click',function(e) {
+  e.stopPropagation();
+});
+
+    $(document).click(function() {
+      console.log($(".sidebar").hasClass('on'));
+      if($(".sidebar").hasClass('on')) {
+        console.log("fuuuck");
+        $('.sidebar').removeClass("on");
+      }
+    });
+});
