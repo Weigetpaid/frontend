9b60c3fa9ca760ab620ba5404df2bbe7dc57c4bd
timmyhadwen <timmyhadwen@gmail.com>
2016-07-31 13:23:40 +1000

    Things

--- js/131940.js
+++ js/131940.js
@@ -3,11 +3,27 @@
 	Road closures
 */
 
+/*
 function getdata_131940(callback) {
 	$.ajax({
-	    url: "http://weigetpaid.timhadwen.com/datasets/traffic.json",
+	    url: "http://131940.qld.gov.au/api/json/v1/events/limits",
+	    type: "GET",
+	    contentType: 'application/json; charset=utf-8',
+	    success: function(resultData) {
+	    	callback(resultData);
+	    },	
+	    timeout: 120000
+	});
+}
+*/
+
+function getdata_131940(callback) {
+	console.log("Requested a thing");
+	$.ajax({
+	    url: "datasets/trafficnew.json",
 	    success: function (data) {
-	        callback(data);
+			console.log(data);
+/* 			callback(JSON.parse(data)); */
 	    }
 	});
 }
