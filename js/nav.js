89a74b8578ff7b015b15bcb4842e71c2537f1b38
Isaac <isaachill13@gmail.com>
2016-07-31 12:20:25 +1000

    new nav bar

--- js/nav.js
+++ js/nav.js
@@ -4,16 +4,19 @@ if (sessionStorage.getItem("lastLat") === null
 }
 
 $( document ).ready(function() {
-    $(".burger").click(function() {
-      if ($(".sidebar").hasClass('on')) {
-        $(".sidebar").removeClass("on");
+    $(".burger.left").click(function() {
+      $(".sidebar.right").removeClass("on");
+      $(".burger.right").addClass('blackBurger');
+      $(".burger.right").removeClass('whiteBurger');
+      if ($(".sidebar.left").hasClass('on')) {
+        $(".sidebar.left").removeClass("on");
       } else {
-        $(".sidebar").addClass("on");
+        $(".sidebar.left").addClass("on");
       }
     });
 
-    $(".burger").click(function() {
-      if ($(".sidebar").hasClass('on')) {
+    $(".burger.left").click(function() {
+      if ($(".sidebar.left").hasClass('on')) {
         $("#burgerMenu").addClass('whiteBurger');
         $("#burgerMenu").removeClass("blackBurger");
       } else {
@@ -24,14 +27,16 @@ $( document ).ready(function() {
 
 
 
-    $(".burger").on('click',function(e) {
+    $(".burger.left").on('click',function(e) {
   e.stopPropagation();
 });
 
     $(document).click(function() {
       console.log($(".sidebar").hasClass('on'));
       if($(".sidebar").hasClass('on')) {
-        $('.sidebar').removeClass("on");      
+        $(".burger").addClass('blackBurger');
+        $(".burger").removeClass('whiteBurger');
+        $('.sidebar').removeClass("on");
       }
     });
 });
